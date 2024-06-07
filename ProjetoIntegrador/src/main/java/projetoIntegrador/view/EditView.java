6421c37c9c460255856d4fafb528e3ba596b2174
Daniel Douglas <danieldouglas26@outlook.com>
2024-06-07 20:16:48 -0300

    Alinhando botões da view de pesquisa

---
+++ ProjetoIntegrador/src/main/java/projetoIntegrador/view/EditView.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package projetoIntegrador.view;
+
+import java.io.IOException;
+
+import org.apache.commons.codec.binary.StringUtils;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author Daniel Douglas <danieldouglas26@outlook.com>
+ */
+public class EditView{
+
+   
+    public void initialize() {
+    }    
+    
+}
